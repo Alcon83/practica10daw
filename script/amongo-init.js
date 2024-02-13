55e18575ff6c97b871295284b0b5eecb3a8fd2bc
Rafa <ambaron@gmail.com>
2024-02-13 13:45:57 +0100

    Ultimas modificaciones solucionando errores

--- script/amongo-init.js
+++ script/amongo-init.js
@@ -2,12 +2,10 @@ db = db.getSiblingDB('movie');
 
 db.createUser({
   user: 'usuario',
-  pwd: 'usuario',
+  pwd: 'usuario', 
   roles: [
     { role: 'dbAdmin', db: 'movie' },
     { role: 'readWrite', db: 'movie' }
   ],
 });
 
-
-db.createCollection("movies");
--- script/restore-dump.sh
+++ script/restore-dump.sh
@@ -2,5 +2,5 @@
   sleep 1
 
 
-
+# Ahora realiza la restauración
 mongorestore --uri="mongodb://usuario:usuario@localhost:27017/movie" db-dump/movie
